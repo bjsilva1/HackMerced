efa344ca38d3278800cf1f54216b7140ccacd961
Bryan Silva <bryanjsilva04@gmail.com>
2024-03-09 08:07:39 -0800

    Added enemy health

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public float enemySpeed = 5;
+    public float enemyHealth = 1;
+
+    float currentHealth;
     Transform player;
     Rigidbody rb;
     
@@ -13,6 +17,7 @@ public class EnemyController : MonoBehaviour
     {
         player = GameObject.Find("Player").GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
+        currentHealth = enemyHealth;
     }
 
     // Update is called once per frame
@@ -21,5 +26,18 @@ public class EnemyController : MonoBehaviour
         Vector3 direction = player.position - transform.position;
         direction = (new Vector3(direction.x, 0, direction.z)).normalized;
         rb.velocity = direction * enemySpeed + new Vector3(0, rb.velocity.y, 0);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject == player.gameObject)
+        {
+            currentHealth--;
+        }
+
+        if (currentHealth < 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
